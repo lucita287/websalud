d399921b5fb2b2afb11fb26d8072ddc920110440
lucita287@gmail.com <lucita287@gmail.com@280d9bba-f494-37c2-1e93-baa3e17bb335>
2012-02-01 08:18:22 +0000

    tabla-prueba

--- trunk/src/servlet/SEditContenido.java
+++ trunk/src/servlet/SEditContenido.java
@@ -1,23 +1,31 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import data.CMenu;
+
+import framework.CDataBase;
+
 /**
- * Servlet implementation class SContenido
+ * Servlet implementation class SEditContenido
  */
-@WebServlet("/SContenido")
-public class SContenido extends HttpServlet {
+@WebServlet("/SEditContenido")
+public class SEditContenido extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SContenido() {
+	
+	
+    public SEditContenido() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,14 +34,21 @@ public class SContenido extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		 response.setContentType("text/html;charset=UTF-8"); 
+			PrintWriter out = response.getWriter();
+			 CDataBase dbo=new CDataBase();
+			 dbo.Connect();
+			 	
+			 String info="{ total: 2, page: 1, rows: [{ cell: [\"test\", \"localhost\" , 8080 , \"ERROR\" , 0.0 ] }, { cell: [\"drew\" , \"localhost\" , 8080 , \"UNKNOWN\" , 0.0 ]}] }";
+			 out.println(info);
+			 
 	}
 
 }
