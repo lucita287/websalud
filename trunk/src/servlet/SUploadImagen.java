6c6f7a767f5b58ea3eb6fd68ee689c2d1acc91a4
lucita287@gmail.com <lucita287@gmail.com@280d9bba-f494-37c2-1e93-baa3e17bb335>
2012-02-14 06:26:24 +0000

    prueba basica subir archivos

--- trunk/src/servlet/SUploadImagen.java
+++ trunk/src/servlet/SUploadImagen.java
@@ -1,11 +1,20 @@
 package servlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet implementation class SUploadImagen
@@ -19,21 +28,57 @@ public class SUploadImagen extends HttpServlet {
      */
     public SUploadImagen() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		if (!ServletFileUpload.isMultipartContent(request)) {
+			throw new IllegalArgumentException("Request is not multipart, please 'multipart/form-data' enctype for your form.");
+		}
+		
+		
+		ServletFileUpload uploadHandler = new ServletFileUpload(new DiskFileItemFactory());
+		PrintWriter writer = response.getWriter();
+		response.setContentType("text/plain");
+			
+		
+		///////////ESCRIBIR ARCHIVOS
+		try {
+			
+			List<FileItem> items = uploadHandler.parseRequest(request);
+			for (FileItem item : items) {
+				if (!item.isFormField()) {
+					String name=item.getName();
+					File file2 = new File(name);
+					name=file2.getName();
+					File file = new File("C:\\Users\\lucita\\Desktop\\up\\"+name);
+					item.write(file);
+					writer.write("{\"name\":\""+ name + "\",\"type\":\"" + item.getContentType() + "\",\"size\":\"" + item.getSize() + "\"}");
+					//break; // assume we only get one file at a time
+				} else {
+			        System.out.println("File field " + item.getString() + " with file name "
+			            + item.getFieldName() + " detected.");
+			        
+			    }
+			}
+		} catch (FileUploadException e) {
+			throw new RuntimeException(e);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			writer.close();
+		}
+
 	}
 
 }
